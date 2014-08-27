d32ecc10e62cfe79b635b40e941d916fceebba51
Leo Antoli <leo.antoli@gmail.com>
2014-08-27 17:46:41 +0200

    euler 7

---
+++ 007_10001st prime.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func IsPrime(prevPrimes []int, candidate int) bool {
+	for _, prime := range prevPrimes {
+		if candidate % prime == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+
+	const total = 10001
+
+	primes := make([]int, 0, total)
+
+	for num := 2 ; len(primes) < total ; num++ {
+		if IsPrime(primes, num) {
+			primes = append(primes, num)
+		}
+	}
+	fmt.Println(primes[len(primes)-1])
+}
